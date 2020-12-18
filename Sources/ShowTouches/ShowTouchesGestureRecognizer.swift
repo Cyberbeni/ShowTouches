eb65c9fd624df7255f9c058b0a7ef2f81d9f362f
Benedek Kozma <cyberbeni@gmail.com>
2020-12-18 22:10:02 +0000

    Commit from GitHub Actions (Format PRs)

--- Sources/ShowTouches/ShowTouchesGestureRecognizer.swift
+++ Sources/ShowTouches/ShowTouchesGestureRecognizer.swift
@@ -1,41 +1,41 @@
 import UIKit
 
 public class ShowTouchesGestureRecognizer: UIGestureRecognizer, UIGestureRecognizerDelegate {
-    let touchesShowingController = ShowTouchesController()
-    
-    public init() {
-        super.init(target: nil, action: nil)
-        self.cancelsTouchesInView = false
-        self.delaysTouchesBegan = false
-        self.delaysTouchesEnded = false
-        self.delegate = self
-    }
-    
-    override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
-        for touch in touches {
-            self.touchesShowingController.touchBegan(touch, view: self.view!)
-        }
-    }
-    
-    override public func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
-        for touch in touches {
-            self.touchesShowingController.touchMoved(touch, view: self.view!)
-        }
-    }
-    
-    override public func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
-        for touch in touches {
-            self.touchesShowingController.touchEnded(touch, view: self.view!)
-        }
-    }
-    
-    override public func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
-        for touch in touches {
-            self.touchesShowingController.touchEnded(touch, view: self.view!)
-        }
-    }
-    
-    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
-    }
+	let touchesShowingController = ShowTouchesController()
+
+	public init() {
+		super.init(target: nil, action: nil)
+		cancelsTouchesInView = false
+		delaysTouchesBegan = false
+		delaysTouchesEnded = false
+		delegate = self
+	}
+
+	override public func touchesBegan(_ touches: Set<UITouch>, with _: UIEvent) {
+		for touch in touches {
+			touchesShowingController.touchBegan(touch, view: view!)
+		}
+	}
+
+	override public func touchesMoved(_ touches: Set<UITouch>, with _: UIEvent) {
+		for touch in touches {
+			touchesShowingController.touchMoved(touch, view: view!)
+		}
+	}
+
+	override public func touchesEnded(_ touches: Set<UITouch>, with _: UIEvent) {
+		for touch in touches {
+			touchesShowingController.touchEnded(touch, view: view!)
+		}
+	}
+
+	override public func touchesCancelled(_ touches: Set<UITouch>, with _: UIEvent) {
+		for touch in touches {
+			touchesShowingController.touchEnded(touch, view: view!)
+		}
+	}
+
+	public func gestureRecognizer(_: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith _: UIGestureRecognizer) -> Bool {
+		true
+	}
 }
